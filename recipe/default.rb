9faec25587416be871d0f1754e2b1706d81c1c3c
Leonardo Alvarez de la Campa Ovies <126499441+Decaxzm@users.noreply.github.com>
2024-10-19 00:12:37 -0500

    Update default.rb

--- recipe/default.rb
+++ recipe/default.rb
@@ -1,14 +1,8 @@
-# recipes/default.rb
-package 'httpd' do
-    action :install
-  end
-  
-  service 'httpd' do
-    action [:enable, :start]
-  end
-  
-  file '/var/www/html/index.html' do
-    content '<html><h1>Hello, World!</h1></html>'
-    action :create
-  end
-  
+# default.rb
+package 'apache2' do
+  action :install
+end
+
+service 'apache2' do
+  action [:enable, :start]
+end
