b3197b8a95ee6fc74e644a0b72b5790c46f6b337
Leonardo Alvarez de la Campa Ovies <126499441+Decaxzm@users.noreply.github.com>
2024-10-18 23:25:44 -0500

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+# metadata.rb
+name 'my-chef-cookbook'
+maintainer 'Your Name'
+maintainer_email 'your_email@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures my-chef-cookbook'
+version '0.1.0'
